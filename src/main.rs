745ea2ed4854284e064b19466ca6f2e4188a2a6d
dwarftee <64mrnoah@gmail.com>
2024-07-27 16:11:37 +0100

    commit :0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let numb = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("guess a number: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse().expect("guess the number!");
+
+        println!("you guessed: {}",guess);
+
+        match guess.cmp(&numb) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
--- target/debug/deps/beans-9e2103185c1f84e0.d
+++ target/debug/deps/beans-9e2103185c1f84e0.d
@@ -1,5 +1,5 @@
-/Users/Noah/Documents/beans-crypt/beans/target/debug/deps/beans-9e2103185c1f84e0: src/main.rs
+/Users/Noah/Documents/beans-crypt/target/debug/deps/beans-9e2103185c1f84e0: src/main.rs
 
-/Users/Noah/Documents/beans-crypt/beans/target/debug/deps/beans-9e2103185c1f84e0.d: src/main.rs
+/Users/Noah/Documents/beans-crypt/target/debug/deps/beans-9e2103185c1f84e0.d: src/main.rs
 
 src/main.rs:
